2b0defac105d02694f16e2ba16a00282db787776
fab-ryan <royalfabrice1234@gmail.com>
2024-03-06 13:15:24 +0200

    remove dist folder

--- dist/schemas/commentValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commentValidation = void 0;
-const zod_1 = require("zod");
-exports.commentValidation = zod_1.z.object({
-    comment: zod_1.z.string().min(1),
-    email: zod_1.z.string().email(),
-    name: zod_1.z.string().min(1),
-});
--- dist/types/modelTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Role;
-(function (Role) {
-    Role["admin"] = "admin";
-    Role["user"] = "user";
-})(Role || (Role = {}));
